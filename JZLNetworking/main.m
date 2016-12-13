fd7af3b5cc449d0e28f2173c69817d94aa278738
allenjzl <allenjzl@163.com>
2016-12-13 14:02:54 +0800

    项目搭建

---
+++ JZLNetworking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JZLNetworking
+//
+//  Created by allenjzl on 2016/12/13.
+//  Copyright © 2016年 allenjzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '8.0'
+
+target 'JZLNetworking' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for JZLNetworking
+ pod 'AFNetworking', '~> 3.1.0'
+end
